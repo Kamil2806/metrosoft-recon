d9254b6dbce4d0a15fa7e15a84d665128b4de4eb
kamils <kserafin2806@gmail.com>
2018-12-16 19:25:13 +0100

    2 - add login form

---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
+import {StateMatcherService} from "../state-matcher.service";
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent {
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  matcher = new StateMatcherService();
+}
